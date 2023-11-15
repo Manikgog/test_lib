1bdf6b8e9407acfcb36d4fe209fa2576f36030b3
Yakimov Lev <tiokuhytblu@gmail.com>
2023-11-15 21:15:15 +0500

    Добавьте файлы проекта.

---
+++ test_lib/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ds.test.impl;
+
+namespace test_lib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPluginFactory factory = new Plugins();
+
+            string[] namesOfActions = factory.GetPluginNames;
+
+            int num1 = 4;
+            int num2 = 2;
+
+            foreach(string name in namesOfActions)
+            {
+                Console.WriteLine(factory.GetPlugin(name).PluginsName);
+                Console.WriteLine(num1 + " " + num2 + " = " + factory.GetPlugin(name).Run(num1, num2));
+            }
+
+
+
+
+        }
+    }
+}
